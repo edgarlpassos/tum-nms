20d0bbff2fa68479277bb911584837e9391f152d
Edgar Passos <edgarlpassos@gmail.com>
2019-06-20 18:12:51 +0200

    [Setup] Set up base package
    - Initialize NPM package and add babelrc
    - Create a "Hello World!" page to deploy to the server
    - Configuration changes on `serverless.yml`

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tum-nms
 Repository for the project for the Networked Multimedia Systems class at Technische Universität München
+
+# Developing
+
+Run the dev server with:
+
+```
+npm run dev
+```
+
+Deploy the application using:
+
+```
+npm run deploy
+```
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
+const pathMatch = require('path-match');
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const { parse } = require('url');
+
+const server = express();
+const route = pathMatch();
+
+server.use('/_next', express.static(path.join(__dirname, '.next')));
+server.get('/', (req, res) => app.render(req, res, '/'));
+
+server.get('*', (req, res) => handle(req, res));
+
+module.exports = server;
