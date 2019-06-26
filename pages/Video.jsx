ea2ff398abd42c3d4164debeb943ce63207687d4
Lolcodes <lolcodes1337@gmail.com>
2019-06-26 15:01:50 +0200

    init Video component

---
+++ pages/Video.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+export default class Video extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  static defaultProps = {
+    poster: null,
+    src: null,
+    width: 640,
+    height: 360,
+    className: null,
+    controls: true,
+    loop: true,
+    autoPlay: true,
+    preload: 'auto',
+  }
+
+  createVideoElem() {
+    return(
+      <video
+        width     = { this.props.width }
+        height    = { this.props.height }
+        className = { this.props.className }
+        loop      = { this.props.loop }
+        autoPlay  = { this.props.autoplay }
+        controls  = { this.props.controls }
+        preload   = { this.props.auto }
+      >
+        <source src={ this.props.src } type={ this.props.type } />
+        Your browser does not support the video tag.
+      </video>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Video.jsx</h2>
+        { this.createVideoElem() }
+      </div>
+    );
+  }
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Video from './Video';
+
 export default () => (
+  <div>
     <div>Hello, World!</div>
+    <Video src="/static/media/vid/sample_video.mp4" type="video/mp4"/>
+  </div>
 );
