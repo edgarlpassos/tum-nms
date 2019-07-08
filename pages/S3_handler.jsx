2520fd8406226fef3ffd2c85de2c573d327f9111
Lolcodes <lolcodes1337@gmail.com>
2019-07-08 17:39:21 +0200

    Quick fix - require statements at top of file - rm static files

--- pages/S3_handler.jsx
+++ pages/S3_handler.jsx
@@ -1,13 +1,12 @@
 const secrets = require('../secrets.json');
+const AWS = require('aws-sdk');
 
 const credentials = {
   accessKeyId:     secrets.ACCESS_KEY_ID,
   secretAccessKey: secrets.SECRET_KEY_ID,
   region: 'eu-central-1',
 };
-
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3(credentials);
+const s3 = new AWS.S3(credentials);
 
 
 export function getURL(bucket, key) {
