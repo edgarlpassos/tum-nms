8d7a534244bb377ed15cb6b62fe0337539b6bd06
GustavJaner <gustav.janer@gmail.com>
2019-07-18 11:15:00 +0200

    [User API] Add API for listUser and getUser - GET listUser, path=users - GET getUser,  path=users/{id}

---
+++ listUser.js
@@ -0,0 +1,39 @@
+import Sequelize from 'sequelize';
+import User from './db/models/user';
+import db from './config/db';
+
+export async function main(event, context, callback) {
+  // Do not wait for sequelize connection to close
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  };
+
+  User.init(db);
+
+  let response;
+  let statusCode;
+
+  try {
+    const result = await User.findAll();
+    statusCode = 200;
+
+    response = {
+      statusCode,
+      headers,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    statusCode = 500;
+
+    response = {
+      statusCode,
+      headers,
+      body: JSON.stringify(error),
+    };
+  }
+
+  callback(null, response);
+}
