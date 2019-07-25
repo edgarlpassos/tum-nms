fb663d927fecdb2c5ef3fd8d237c926026b31c19
GustavJaner <gustav.janer@gmail.com>
2019-07-25 03:46:11 +0200

    [ListComment] Hotfix casting n_likes and liked to int

--- functions/api/listComment.js
+++ functions/api/listComment.js
@@ -16,11 +16,11 @@ export async function main(event, context, callback) {
 
   try {
     const result = await db.query(
-      `SELECT id, content, video, timestamp, created_by, username, coalesce(n_likes, 0) AS n_likes, coalesce(liked, 0) liked, "createdAt", "updatedAt" FROM \
+      `SELECT id, content, video, timestamp, created_by, username, COALESCE(n_likes, 0) AS n_likes, COALESCE(liked, 0) AS liked, "createdAt", "updatedAt" FROM \
         (SELECT id, content, video, timestamp, created_by, "createdAt", "updatedAt" FROM public."Comments" WHERE video = ${videoId}) a \
-        LEFT JOIN ( SELECT comment, user, count(*) AS n_likes FROM public."Likes" GROUP BY comment ) c ON a.id = c.comment \
+        LEFT JOIN ( SELECT comment, user, CAST(COUNT(*) AS int) AS n_likes FROM public."Likes" GROUP BY comment ) c ON a.id = c.comment \
         JOIN (SELECT id AS user_id, username, picture FROM public."Users") d ON a.created_by = d.user_id \
-        LEFT JOIN (SELECT COUNT(*) AS liked, comment from public."Likes" where "user" = ${userId} group by comment) e on a.id = e.comment \
+        LEFT JOIN (SELECT CAST(COUNT(*) AS int) AS liked, comment FROM public."Likes" WHERE "user" = ${userId} GROUP BY comment) e ON a.id = e.comment \
       ORDER BY "createdAt" DESC;`,
       {type: db.QueryTypes.SELECT}
     );
